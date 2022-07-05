073c403e8766e301f9c379287157a69e0428edbe
AlexKuaiR <alexk201512@email.com>
2022-07-05 17:15:47 -0400

    Added graph and skeleton for description and citation.

---
+++ setup.R
@@ -0,0 +1,23 @@
+library(rvest)
+library(janitor)
+library(tidyverse)
+library(readxl)
+
+market <- read_xls(path = "WSTS-Historical-Billings-Report-April2022data.xls") |> as_tibble()
+
+
+market_new <- market |> 
+  row_to_names(row_number = 3)
+
+
+colnames(market_new) <- c("x", "January", "February","March","April", "May","June" ,
+                          "July" ,"August","September","October", "November", "December", "Total_Year",
+                          "Q1" , "Q2" , "Q3", "Q4" )
+
+market_data <- market_new |> 
+  mutate(year = rep(1986:2022, each = 6))|>
+  filter(!year %in% 2022, x != "Worldwide") |>
+  mutate(Total_Year = as.integer(Total_Year) / 1000000) |> 
+  drop_na()
+
+write_rds(market_data, file = "rawdata.rds")
